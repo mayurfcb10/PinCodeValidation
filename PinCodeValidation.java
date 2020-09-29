a541046bc89e937912c271b66733acd155391ad3
Mayur <mayurzope555@gmail.com>
2020-09-29 17:21:22 +0530

    [Mayur]ADDED: Pin Code Validation Use Case 3 restricting the alphabet at ending

--- PinCodeValidation.java
+++ PinCodeValidation.java
@@ -4,36 +4,50 @@ import java.util.regex.*;
 import java.util.*;
 
 public class PinCodeValidation {
-	
+
 	static final Scanner sc = new Scanner(System.in);
-	
+
 	public static void ValidatePinUC1(String pinCode) {
-		
+
 		boolean matcher = pinCode.matches("[\\d ]{6}");
-		
+
 		if(matcher == true) {
 			System.out.println("Valid PIN Code");
 		}else {
 			System.out.println("Invalid PIN Code");
 		}
 	}
-	
+
 	public static void ValidatePinUC2(String pinCode) {
-		
+
 		boolean matcher = pinCode.matches("^[\\d ]{6}");
-		
+
 		if(matcher == true) {
 			System.out.println("Valid PIN Code");
 		}else {
 			System.out.println("Invalid PIN Code");
 		}
+	}
+
+	public static void ValidatePinUC3(String pinCode) {
+
+		boolean matcher = pinCode.matches("^[\\d ]{6}$");
+
+		if(matcher == true) {
+			System.out.println("Valid PIN Code");
+		}else {
+			System.out.println("Invalid PIN Code");
+		}
+
 	}
 	public static void main(String[] args) {
-		
+
 		System.out.println("Welcome to the Pin Code Validation Problem");
 		System.out.println("Enter the Pin Code to be Validated");
 		String pinCode = sc.nextLine();
-		ValidatePinUC2(pinCode);
+		ValidatePinUC3(pinCode);
 
 	}
+
 }
+
