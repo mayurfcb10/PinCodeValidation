a0661e2fd53445290707acd23b030226e16797c7
Mayur <mayurzope555@gmail.com>
2020-09-29 17:18:22 +0530

    [Mayur]ADDED: Pin Code Validation Use Case 2 restricting the alphabet at beginning

--- PinCodeValidation.java
+++ PinCodeValidation.java
@@ -4,26 +4,36 @@ import java.util.regex.*;
 import java.util.*;
 
 public class PinCodeValidation {
-
+	
 	static final Scanner sc = new Scanner(System.in);
-
+	
 	public static void ValidatePinUC1(String pinCode) {
-
+		
 		boolean matcher = pinCode.matches("[\\d ]{6}");
-
+		
 		if(matcher == true) {
 			System.out.println("Valid PIN Code");
 		}else {
 			System.out.println("Invalid PIN Code");
 		}
-
+	}
+	
+	public static void ValidatePinUC2(String pinCode) {
+		
+		boolean matcher = pinCode.matches("^[\\d ]{6}");
+		
+		if(matcher == true) {
+			System.out.println("Valid PIN Code");
+		}else {
+			System.out.println("Invalid PIN Code");
+		}
 	}
 	public static void main(String[] args) {
-
+		
 		System.out.println("Welcome to the Pin Code Validation Problem");
 		System.out.println("Enter the Pin Code to be Validated");
 		String pinCode = sc.nextLine();
-		ValidatePinUC1(pinCode);
+		ValidatePinUC2(pinCode);
 
 	}
 }
