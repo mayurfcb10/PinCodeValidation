95f521a5bc1e4d081f88a8b960c387df7102733a
Mayur <mayurzope555@gmail.com>
2020-09-29 17:24:47 +0530

    [Mayur]ADDED: Pin Code Validation Use Case 4 accepting space between the pattern

--- PinCodeValidation.java
+++ PinCodeValidation.java
@@ -1,5 +1,3 @@
-/* Welcome to the PIN Code Validation Problem */
-
 import java.util.regex.*;
 import java.util.*;
 
@@ -40,14 +38,26 @@ public class PinCodeValidation {
 		}
 
 	}
+
+	public static void ValidatePinUC4(String pinCode) {
+
+		boolean matcher = pinCode.matches("^[0-9]{3}?(\s)[0-9]{3}$");
+		
+		if(matcher == true) {
+			System.out.println("Valid PIN Code");
+		}else {
+			System.out.println("Invalid PIN Code");
+		}
+
+	}
+
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to the Pin Code Validation Problem");
 		System.out.println("Enter the Pin Code to be Validated");
 		String pinCode = sc.nextLine();
-		ValidatePinUC3(pinCode);
+		ValidatePinUC4(pinCode);
 
 	}
 
 }
-
